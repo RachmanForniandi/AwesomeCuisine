c26c030d532f9037b961cd3c7f9ac21053d125f0
RachmanForniandi <rachmanforniandi@gmail.com>
2021-08-24 17:43:24 +0700

    inject remote data source class done.

--- app/src/main/java/rachmanforniandi/awesomecuisine/di/RemoteDataSource.kt
+++ app/src/main/java/rachmanforniandi/awesomecuisine/di/RemoteDataSource.kt
@@ -1,4 +1,13 @@
 package rachmanforniandi.awesomecuisine.di
 
-class RemoteDataSource() {
+import rachmanforniandi.awesomecuisine.models.FoodRecipe
+import rachmanforniandi.awesomecuisine.networkUtils.ApiRecipeService
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(private val recipeService: ApiRecipeService) {
+
+    suspend fun getRecipesData(queries:Map<String,String>):Response<FoodRecipe>{
+        return recipeService.getRecipes(queries)
+    }
 }
