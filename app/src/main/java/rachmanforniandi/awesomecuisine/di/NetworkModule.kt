40afaaeac902333aa64b0ffa66ebf3b73d26583f
RachmanForniandi <rachmanforniandi@gmail.com>
2021-08-24 17:31:52 +0700

    networkModule class done.

---
+++ app/src/main/java/rachmanforniandi/awesomecuisine/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package rachmanforniandi.awesomecuisine.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import rachmanforniandi.awesomecuisine.networkUtils.ApiRecipeService
+import rachmanforniandi.awesomecuisine.networkUtils.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+    @Singleton
+    @Provides
+    fun provideHttpClient():OkHttpClient{
+        return OkHttpClient.Builder()
+            .readTimeout(15,TimeUnit.SECONDS)
+            .connectTimeout(15,TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstanceNet(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ):Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit):ApiRecipeService{
+        return retrofit.create(ApiRecipeService::class.java)
+    }
+}
