dcbf2086e52438726d130cf03f711406aa80d673
RachmanForniandi <rachmanforniandi@gmail.com>
2022-08-17 20:19:26 +0700

    added recipes entity done.

---
+++ app/src/main/java/rachmanforniandi/awesomecuisine/database/RecipesEntity.kt
@@ -0,0 +1,13 @@
+package rachmanforniandi.awesomecuisine.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import rachmanforniandi.awesomecuisine.models.FoodRecipe
+import rachmanforniandi.awesomecuisine.networkUtils.Constants
+import rachmanforniandi.awesomecuisine.networkUtils.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity (var foodRecipe: FoodRecipe){
+    @PrimaryKey(autoGenerate = false)
+    var id:Int =0
+}
