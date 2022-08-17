517134a693ea772d612f1fa77d95ed8f2303d57c
RachmanForniandi <rachmanforniandi@gmail.com>
2022-08-17 22:09:53 +0700

    create & inject local datasource done.

--- app/src/main/java/rachmanforniandi/awesomecuisine/repo/Repository.kt
+++ app/src/main/java/rachmanforniandi/awesomecuisine/repo/Repository.kt
@@ -1,13 +1,16 @@
 package rachmanforniandi.awesomecuisine.repo
 
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import rachmanforniandi.awesomecuisine.data.LocalDataSource
 import rachmanforniandi.awesomecuisine.data.RemoteDataSource
 import javax.inject.Inject
 
 @ActivityRetainedScoped
 class Repository @Inject constructor(
-    remoteDataSource: RemoteDataSource
+    remoteDataSource: RemoteDataSource,
+    localDataSource: LocalDataSource
 ) {
 
     val remote = remoteDataSource
+    val local = localDataSource
 }
