e47b7f6182b45cfb1afdaed840ee7d18a4533c3c
RachmanForniandi <rachmanforniandi@gmail.com>
2022-08-20 18:51:22 +0700

    added datastorerepository class done.

--- app/src/main/java/rachmanforniandi/awesomecuisine/viewModel/RecipesViewModel.kt
+++ app/src/main/java/rachmanforniandi/awesomecuisine/viewModel/RecipesViewModel.kt
@@ -3,6 +3,9 @@ package rachmanforniandi.awesomecuisine.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import rachmanforniandi.awesomecuisine.util.Constants
+import rachmanforniandi.awesomecuisine.util.Constants.Companion.DEFAULT_DIET_TYPE
+import rachmanforniandi.awesomecuisine.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import rachmanforniandi.awesomecuisine.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import rachmanforniandi.awesomecuisine.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import rachmanforniandi.awesomecuisine.util.Constants.Companion.QUERY_API_KEY
 import rachmanforniandi.awesomecuisine.util.Constants.Companion.QUERY_DIET
@@ -14,10 +17,10 @@ class RecipesViewModel(application: Application) : AndroidViewModel(application)
 
     fun applyQueries():HashMap<String,String>{
         val queries:HashMap<String,String> = HashMap()
-        queries[QUERY_NUMBER]="50"
+        queries[QUERY_NUMBER]= DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY]= Constants.API_KEY
-        queries[QUERY_TYPE]="main courses"
-        queries[QUERY_DIET]="gluten free"
+        queries[QUERY_TYPE]=DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET]=DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION]="true"
         queries[QUERY_FILL_INGREDIENTS]="true"
         return queries
